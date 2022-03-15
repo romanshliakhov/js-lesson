286e409c721b0a7a832790965c8b18abd3cd1c88
Татьяна Холод <futurequeen@MacBook-Air-Tatana.local>
2022-03-15 19:34:45 +0200

    try to update

---
+++ js/lessons.js
@@ -0,0 +1,23 @@
+// let number = 4; 
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Roman";
+
+// alert(`Hello , ${user}`);
