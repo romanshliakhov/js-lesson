50a69d6f4135a0536e5642cda128edc208849ecd
Roman <romanshliakhov@gmail.com>
2022-04-05 21:46:52 +0300

    finished 21 and 22 exercises

--- js/lessons.js
+++ js/lessons.js
@@ -30,7 +30,7 @@
 
 // console.log(isChecked || !isClose);
 
-// exercise 12-13
+// Lesson  12-13
 
 // if ( 4 == 9) {
 //     console.log('Ok!');
@@ -78,7 +78,7 @@
 //         break;
 // }
 
-// Exercise 14
+// Lesson  14
 
 // let num = 50;
 
@@ -117,7 +117,7 @@
 //     console.log(i);
 // }
 
-// Exercise 16
+// Lesson  16
 // let num = 20;
 
 // function showFirstMessage(text) {
@@ -152,7 +152,7 @@
 
 // const calc = (a, b) => a + b;
 
-// Exercise 17
+// Lesson 17
 // const str = "test";
 // // console.log(str[2] = 'd');
 // console.log(str.toLowerCase());
@@ -173,3 +173,208 @@
 // const test = "12.2px";
 // console.log(parseInt(test));
 // console.log(parseFloat(test));
+
+// Lesson 19
+
+// function first() {
+//     // Do something
+
+//     setTimeout(function () {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function leadrJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();  
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// leadrJS('JavaScript', done);
+
+// Lesson  20
+// const obj = new Object(); // - Одна из устаревших записей
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         background: 'red'
+//     },
+//     makeTest: function() {
+//         console.log("Test");
+//     }
+// }
+
+// options.makeTest();
+
+// const {border, background} = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+
+// console.log(options);
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//        for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//        }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// Lesson  21
+
+// const arr = [2, 13, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function (item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
+
+// Lesson  22
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// }
+
+// const copy = obj; // Ссылка
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (const key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asasa';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'];
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newArray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+
