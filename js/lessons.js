1c6b5317ac6634bd6697cd22ce19fcb7d99418e3
Roman <romanshliakhov@gmail.com>
2022-04-06 21:46:46 +0300

    added lesson 22 and prectice 23

--- js/lessons.js
+++ js/lessons.js
@@ -378,3 +378,32 @@
 
 // const newObj = {...q};
 
+// Lesson  23
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+// const soldier = {
+//     health: 400,
+//     aromor: 100,
+//     sayHello: function() {
+//         console.log("hello");
+//     }
+// };
+
+// const jonh = Object.create(soldier);
+
+// // const jonh = {
+// //     health: 100
+// // };
+
+// // jonh.__proto__ = soldier;
+
+// // Object.setPrototypeOf(jonh, soldier);
+
+// jonh.sayHello();
+
+// console.log(jonh.aromor);
