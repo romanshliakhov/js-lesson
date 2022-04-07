39749facaf10f1be885ef757a5c7ab3b0b16ddab
Roman <romanshliakhov@gmail.com>
2022-04-07 20:41:49 +0300

    finished lesson 28

--- js/lessons.js
+++ js/lessons.js
@@ -483,4 +483,23 @@
 // // 3)
 // console.log(typeof(!!'44444'));
 
-// Lesson  27
+// Lesson  28
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+// console.log(btns[0]);
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+// console.log(hearts);
+
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
