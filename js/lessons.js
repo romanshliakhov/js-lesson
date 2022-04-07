f17e1d5ba9b04bac2fde50df4e519cf45fe2bf9d
Roman <romanshliakhov@gmail.com>
2022-04-07 08:42:39 +0300

    finished lesson 25

--- js/lessons.js
+++ js/lessons.js
@@ -407,3 +407,25 @@
 // jonh.sayHello();
 
 // console.log(jonh.aromor);
+
+// Lesson  25
+// function hello() {
+//     console.log("Hello World");
+// }
+
+// hello();
+
+// function hi() {
+//     console.log("Say hi!");
+// }
+
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+//       sorted = arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// console.log(sorted);
