bc2e900ef261108aebad5501f1395a7ea493474e
Roman <romanshliakhov@gmail.com>
2022-04-09 15:22:41 +0300

    finished lesson 34

--- js/lessons.js
+++ js/lessons.js
@@ -621,3 +621,28 @@
 
 //     console.log(node);
 // }
+
+// Lesson  34
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+
+//         console.log('click');
+//         console.log(e.targetTouches);
+//     });
+
+//     box.addEventListener('touchmove', (e) => {
+//         e.preventDefault();
+
+//         console.log(e.targetTouches[0].pageX);
+//     });
+
+//     box.addEventListener('touchend', (e) => {
+//         e.preventDefault();
+
+//         console.log('end');
+//     });
+// });
