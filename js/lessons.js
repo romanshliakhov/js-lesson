1013b7f29be32530d7af007e6255bb864fb3bd14
Roman <romanshliakhov@gmail.com>
2022-04-09 12:43:56 +0300

    finished lesson 31

--- js/lessons.js
+++ js/lessons.js
@@ -555,3 +555,48 @@
 // div.insertAdjacentHTML('afterend' , '<h2>Hello</h2>')
 
 // // div.textContent = 'Hello';
+
+// Lesson  31
+// const btn = document.querySelector('button'),
+//       btns = document.querySelectorAll('button'),
+//       overlay = document.querySelector('.overlay');
+
+// btn.onclick = function () {
+//     alert('Hello');
+// }
+
+// btn.addEventListener('click', () => {
+//     alert('Hello');
+// });
+
+// btn.addEventListener('click', () => {
+//     alert('Hello World');
+// });
+
+// let i = 0;
+
+// const deleteElement = (e) => {
+//    console.log(e.target);
+//    console.log(e.type);
+// //    i++;
+// //    if (i == 1) {
+// //     btn.removeEventListener('click', deleteElement);
+// //    }
+// };
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', deleteElement, {once: true});
+// });
+
+// const link = document.querySelector('a');
+
+// link.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     console.log(event.target);
+// });
+
+// Lesson  32
