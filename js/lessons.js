fa5c832757b16af76a2607f7245cb378e524da5e
Roman <romanshliakhov@gmail.com>
2022-04-15 10:46:13 +0300

    done homework for 10 lesson

--- js/lessons.js
+++ js/lessons.js
@@ -17,6 +17,39 @@
 // const COLOR_RED = '#fff';
 // cosnt _apeBase = '';
 
+// Lesson  12
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '3456';
+
+// console.log(arr[10]);
+
+// const arrObj = {
+//     a: 'a',
+//     1: 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// }
+
+// const b = 'b';
+
+// // arrObj.b = '1234';
+// arrObj[b]= '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// // const obj = {a: 1, b: 2};
+// const obj = {
+//     Anna: 500,
+//     Alice: 800
+// };
+
 // const result = confirm("Are you here?");
 // console.log(result);
 
