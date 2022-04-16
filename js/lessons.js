17c61e0b5e08d99eb3b744d3287192dce2a780c1
Roman <romanshliakhov@gmail.com>
2022-04-16 20:15:28 +0300

    done homework 23

--- js/lessons.js
+++ js/lessons.js
@@ -225,6 +225,48 @@
 //     console.log(i);
 // }
 
+// Lesson  23
+// for ( let i = 0; i < 3; i++) {
+//     console.log(i);
+
+//     for ( let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+//     for ( let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// first: for ( let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+
+//     for ( let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+
+//         for ( let k = 0; k < 5; k++) {
+//             if ( k === 2) break first;
+
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
 // Lesson  16
 // let num = 20;
 
