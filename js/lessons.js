c94d9b303eaa1b6951d267f1534a1035894e0344
Roman <romanshliakhov@gmail.com>
2022-04-17 09:49:24 +0300

    done lessons 26 and 27

--- js/lessons.js
+++ js/lessons.js
@@ -303,6 +303,48 @@
 
 // const calc = (a, b) => a + b;
 
+// Lesson 26
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert (amount, curr) {
+//     console.log(curr * amount);
+// }
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// Lesson 27
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert (amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion (result) {
+//     console.log(result * discount);
+    
+// }
+
+// const res = convert(500, usdCurr);
+
+// promotion(res);
+
+// function test () {
+//     for ( let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return;
+//     }
+//     console.log('done');
+// }
+
+// test();
+
+// function doNothing () {};
+// console.log(doNothing() === undefined);
+
 // Lesson 17
 // const str = "test";
 // // console.log(str[2] = 'd');
