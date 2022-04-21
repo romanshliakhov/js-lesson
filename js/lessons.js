7652b986ba4572daa6bc1a395360eb295d3c2ca9
Roman <romanshliakhov@gmail.com>
2022-04-21 21:31:08 +0300

    done lessons 52 and 53

--- js/lessons.js
+++ js/lessons.js
@@ -1040,6 +1040,28 @@
 
 
 
+// Lesson 53
+
+// const box = document.querySelector('.box');
+
+// const newHeight = 100;
+// const newWidth = 400;
+
+// function changeParams(elem, h, w) {
+//     elem.style.height = `${h ?? 200}px`;
+//     elem.style.width = `${w ?? 200}px`;
+//     elem.innerHTML = (h ?? 200) * (w ?? 200);
+// }
+
+// changeParams(box, newHeight, newWidth);
+
+// let userName;
+// let userKey;
+
+// console.log(userName ?? userKey ?? 'User');
+
+
+
 
 // Lesson  37
 // const btns = document.querySelectorAll('button');
