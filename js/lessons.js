ce9e267c79f16a6ee7c35a356b7ee9ddfdf648f3
Roman <romanshliakhov@gmail.com>
2022-04-22 23:16:13 +0300

    done lesson 59

--- js/lessons.js
+++ js/lessons.js
@@ -1264,7 +1264,96 @@
 
 
 
-// Lesson  59 
+// Lesson 59
+
+// const user = {
+//     4: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicDate: function () {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
+
+// console.log(user);
+// console.log(typeof(Object.keys(user)[0]));
+
+// У нас есть список магазинов в торговой сети. Каждый из магазинов содержит определенный перечень товаров и на каждый из них выделяется определенный бюджет. 
+
+// const shops = [
+//     {rice: 500},
+//     {oil: 200},
+//     {bread: 50}
+// ]
+
+// const budget = [5000, 15000, 25000];
+
+// const map = new Map([
+//     [{paper: 400}, 8000]
+// ]);
+
+// // оптимальный вариант записи
+// shops.forEach((shop, i) => {
+//     map.set(shop, budget[i]);
+// });
+
+// console.log(map);
+
+// map.set(shops[0], 5000);
+// map.set(shops[1], 15000);
+// map.set(shops[2], 25000);
+// аналогичная запись
+// map.set(shops[0], 5000)
+//     .set(shops[1], 15000)
+//     .set(shops[2], 25000);
+
+
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// map.delete(key);
+// map.clear(key);
+// map.size;
+
+// Способы перебора map
+// map.keys()
+// map.values()
+// map.enteries()
+// map.forEach()
+
+// const goods = [];
+
+// for (let shop of map.keys()) {
+//     console.log(shop);
+//     goods.push(Object.keys(shop)[0]);
+// }
+// console.log(goods);
+
+// for (let price of map.values()) {
+//     console.log(price);
+// }
+
+// for (let [shop, price] of map.entries()) {
+//     console.log(shop, price);
+// }
+
+// map.forEach((value, key, map) => {
+//     console.log(key, value);
+// });
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicDate: function () {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
+
+// const userMap = new Map(Object.entries(user));
+// console.log(userMap);
+
+// const newUserObj = Object.fromEntries(userMap);
+// console.log(newUserObj);
 
 
 
