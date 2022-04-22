37d52ec5adf553b3757770b681a71a2d426a7130
Roman <romanshliakhov@gmail.com>
2022-04-22 16:16:24 +0300

    done lesson 58

--- js/lessons.js
+++ js/lessons.js
@@ -1170,6 +1170,7 @@
 
 
 // Lesson 57
+
 // const birthday = Symbol('birthday');
 
 // const user = {
@@ -1194,14 +1195,79 @@
 //     surname: {writable: false}
 // })
 
-
-
-
-
-
 // Object.defineProperty(user, 'name', {writable: false});
 // Object.defineProperty(user, 'gender', {value: 'male'});
 // console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
+
+
+
+// Lesson 58
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicDate: function () {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
+
+// // for ( const key of user) console.log(user[key]);
+
+// const arr = ['b', 'a', 'c'];
+// Array.prototype.someMethod = function () {};
+
+
+// console.dir(arr);
+
+// for ( const key of arr) {
+//     console.log(key);
+// }
+
+// const str = 'string';
+
+// for ( const key in str) {
+//     console.log(str[key]);
+// }
+
+// const salaries = {
+//     john: 500,
+//     ivan: 1000,
+//     ann: 5000,
+//     sayHello: function () {
+//         console.log('Hello');
+//     }
+// }
+
+// salaries[Symbol.iterator] = function () {
+//     return {
+//         current: this.john,
+//         last: this.ann,
+
+//         next() {
+//             if (this.current < this.last) {
+//                 this.current = this.current + 500;
+//                 return {done: false, value: this.current};
+//             } else {
+//                 return {done: true};
+//             }
+//         }
+//     };
+// }
+
+// for (let res of salaries) {
+//     console.log(res);
+// }
+
+// const iterator = salaries[Symbol.iterator]();
+// console.log(iterator.next());
+
+
+
+// Lesson  59 
+
+
+
 
 
 
