acb24309b878d9a8c986d5b2a9b52de6d280e096
Roman <romanshliakhov@gmail.com>
2022-04-24 15:42:02 +0300

    done lesson 71

--- js/lessons.js
+++ js/lessons.js
@@ -1613,3 +1613,31 @@
 // let end = new Date();
 
 // alert(`Цикл отработал за ${end - start} миллисекунд`);
+
+
+
+// Lesson  71
+// const box = document.querySelector('.box'),
+//       btn = document.querySelector('button');
+
+// // const width = box.clientWidth;
+// // const height = box.clientHeight;
+// // const width = box.offsetWidth;
+// // const height = box.offsetHeight;
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
+
+// console.log(width, height);
+
+// btn.addEventListener('click', () => {
+//     // box.style.height = box.scrollHeight + 'px';
+//     console.log(box.scrollTop);
+// });
+
+// console.log(box.getBoundingClientRect().top);
+
+// const style = window.getComputedStyle(box);
+
+// console.log(style.display);
+
+// console.log(document.documentElement.scrollTop);
