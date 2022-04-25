9d88b5e1586eee44621b52d8be3a8d534b5935fd
Roman <romanshliakhov@gmail.com>
2022-04-25 09:56:47 +0300

    done lesson 75

--- js/lessons.js
+++ js/lessons.js
@@ -1658,3 +1658,35 @@
 // });
 
 // observer.disconnect();
+
+
+
+
+// Lesson  75
+
+// const num = new Number(3);
+// console.log(num);
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} ушел`)
+// }
+
+// ivan.exit();
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
