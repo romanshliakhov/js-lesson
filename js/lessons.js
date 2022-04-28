47c761681054012ae1af20001655963973765ebc
Roman <romanshliakhov@gmail.com>
2022-04-28 18:10:56 +0300

    done lesson 88

--- js/lessons.js
+++ js/lessons.js
@@ -1980,4 +1980,55 @@
 
 
 
+// Lesson 88
 
+// filter 
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+
+// map 
+
+// const answers = ['IvAn', 'ANNA', 'Hello'];
+
+// const result = answers.map(item => item.toLocaleLowerCase());
+// console.log(result);
+
+
+// every/some
+
+// const some = [4, 'qwwe', 'afafaf'];
+
+// // console.log(some.some(item => typeof(item) === 'number'));
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// reduce 
+
+// const arr =[4, 5, 1, 3, 2, 6];
+
+// const result = arr.reduce((sum, current) => sum + current);
+// console.log(result);
+
+// const arr =['apple', 'pear', 'plum'];
+
+// const result = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(result);
+
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+
+// const newArr = Object.entries(obj)
+// .filter(item => item[1] === 'persone')
+// .map(item => item[0]);
+
+// console.log(newArr);
