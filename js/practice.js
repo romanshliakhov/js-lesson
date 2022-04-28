84cd52bfcedbfe4c52d6782f6d0e37fc8278c6d9
Roman <romanshliakhov@gmail.com>
2022-04-28 19:07:59 +0300

    done homework 88

--- js/practice.js
+++ js/practice.js
@@ -1189,3 +1189,102 @@
 //     for (let i=0; i<a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
 //     return count;
 // }
+
+
+
+
+// Домашнее задание к уроку 88
+
+// Задача 1
+// 1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать  будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать  будет строку, которая содержит названия фильмов через запятую.
+// Пример: 
+// showListOfFilms(films) =>  "Titanic, Die hard 5, Matrix, Some bad film"
+// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма. 
+// Пример: 
+// setFilmsIds(films) =>  [
+//     { name: 'Titanic', rating: 9, id: 0 },
+//     { name: 'Die hard 5', rating: 5, id: 1 },
+//     { name: 'Matrix', rating: 8, id: 2 },
+//     { name: 'Some bad film', rating: 4, id: 3 }
+// ]
+// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом  из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray);
+
+// const films = [
+//     {
+//         name: 'Titanic',
+//         rating: 9
+//     },
+//     {
+//         name: 'Die hard 5',
+//         rating: 5
+//     },
+//     {
+//         name: 'Matrix',
+//         rating: 8
+//     },
+//     {
+//         name: 'Some bad film',
+//         rating: 4
+//     }
+// ];
+
+// function showGoodFilms(arr) {
+//     return arr.filter(film => film.rating >= 8);
+// }
+
+// console.log(showGoodFilms(films));
+
+// function showListOfFilms(arr) {
+//     return arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
+// }
+
+// showListOfFilms(films);
+// console.log(showListOfFilms(films));
+
+// function setFilmsIds(arr) {
+//     return arr.map((film, i) => {
+//         film.id = i;
+//         return film;
+//     });
+// }
+
+// const tranformedArray = setFilmsIds(films);
+// console.log(tranformedArray);
+
+// function checkFilms(arr) {
+//     return arr.every(film => film.id || film.id === 0 ? true : false)
+// }
+
+// checkFilms(tranformedArray);
+
+// console.log(checkFilms(tranformedArray));
+
+// Задача 2
+
+// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+// Пример:
+// getPositiveIncomeAmount(funds) => 13300
+// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех  значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+// Пример:
+// getTotalIncomeAmount(funds) => -500
+// const funds = [
+//     {amount: -1400},
+//     {amount: 2400},
+//     {amount: -1000},
+//     {amount: 500},
+//     {amount: 10400},
+//     {amount: -11400}
+// ];
+
+// const getPositiveIncomeAmount = (data) => {
+//     return data.filter(item => item.amount > 0).reduce((sum, current) => sum + current.amount, 0);
+// };
+
+// console.log(getPositiveIncomeAmount(funds));
+
+// const getTotalIncomeAmount = (data) => {
+//     return data.some(item => item.amount > 0) ? data.reduce((sum, current) => sum + current.amount, 0) : getPositiveIncomeAmount();
+// };
+
+// console.log(getTotalIncomeAmount(funds));
