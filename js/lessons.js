991958d98467e0e2b2362e8cb8253edf7cd3d49b
Roman <romanshliakhov@gmail.com>
2022-04-30 17:31:32 +0300

    done lessons 95 and 96

--- js/lessons.js
+++ js/lessons.js
@@ -2032,3 +2032,74 @@
 // .map(item => item[0]);
 
 // console.log(newArr);
+
+
+// Lesson 95
+
+// localStorage.setItem('number', 5);
+// localStorage.removeItem();
+// localStorage.clear();
+
+// console.log(localStorage.getItem('number'));
+
+// const checkbox = document.querySelector('#checkbox'),
+//       form = document.querySelector('form'),
+//       change = document.querySelector('#color');
+
+// if (localStorage.getItem('isChecked')) {
+//     checkbox.checked = true;
+// }
+
+// if (localStorage.getItem('bg' === 'changed')) {
+//     form.style.backgroundColor = 'red';
+// } 
+
+// checkbox.addEventListener('change', () => {
+//     localStorage.setItem('isChecked', true);
+// });
+
+// change.addEventListener('click', () => {
+//     if (localStorage.getItem('bg' === 'changed')) {
+//         localStorage.removeItem('bg');
+//         form.style.backgroundColor = 'fff';
+//     } else {
+//         localStorage.setItem('bg', 'changed');
+//         form.style.backgroundColor = 'red';
+//     }
+// });
+
+// const persone = {
+//     name: 'Alex',
+//     age: 25
+// }
+
+// const serializedPersone = JSON.stringify(persone);
+// localStorage.setItem('alex', serializedPersone);
+
+// console.log(JSON.parse(localStorage.getItem('alex')));
+
+
+
+// Lesson 96
+
+// new RegExp('pattern', 'flags');
+// /pattern/flags
+
+// const ans = prompt('Введите ваше число');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+// const str = 'My name is R2D2';
+// console.log(str.match(/\w\d\w\d/i));
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+// console.log(ans.replace(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, '*'));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
