b29d8977839c37e6037a34b8c070d9ef781d9dca
Roman <romanshliakhov@gmail.com>
2022-05-01 18:09:37 +0300

    done lesson 101

--- js/lessons.js
+++ js/lessons.js
@@ -2187,3 +2187,30 @@
 // const roman = new User('Roman', 30);
 // console.log(roman.surname);
 // roman.say();
+
+
+
+// Lesson 101 
+
+// const app = '123';
+
+// const number = 1;
+
+// (function(){
+//     let number = 2;
+//     console.log(number);
+//     console.log(number + 3);
+// }());
+
+// console.log(number);
+
+// const user = (function(){
+//     const privat = function() {
+//         console.log('I am privat');
+//     };
+
+//     return {
+//         sayHello: privat
+//     };
+// }());
+// user.sayHello();
