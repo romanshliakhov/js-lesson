88862121b2be803ec23e196818388c0782a5d3e5
Roman <romanshliakhov@gmail.com>
2022-05-03 14:26:43 +0300

    done lessons 105-107

--- js/lessons.js
+++ js/lessons.js
@@ -2214,3 +2214,59 @@
 //     };
 // }());
 // user.sayHello();
+
+
+
+// Lesson 106
+
+// try {
+//     console.log('Normal');
+//     console.log(name);
+//     console.log('result');
+// } catch (error) {
+//     console.log(error);
+// } finally {
+
+// }
+
+// console.log(a);
+// console.log('I am here');
+
+
+
+
+
+// Lesson 107 
+
+// const data = [
+//     {
+//         id: 'box',
+//         tag: 'div'
+//     },
+//     {
+//         id: 'fff',
+//         tag: 'nav'
+//     },
+//     {
+//         id: 'circle',
+//         tag: ''
+//     }
+// ]
+
+// try {
+//     data.forEach((blockObj, i) => {
+//         const block = document.createElement(blockObj.tag);
+    
+//         if (!blockObj.id) throw new SyntaxError(`В данних под номером ${i + 1} нет id`);
+    
+//         block.setAttribute('id', blockObj.id);
+//         document.body.append(block);
+//     })
+// } catch(e) {
+//     if (e.name === 'SyntaxError') {
+//         console.log(e.message);
+//     } else throw e;
+// }
+
+// const err = new SyntaxError('lorem');
+// console.log(err.name, err.message, err.stack);
