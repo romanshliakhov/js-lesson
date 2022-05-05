752665d44900515a36b6f4aa8e7d935fe1173d3d
Roman <romanshliakhov@gmail.com>
2022-05-05 10:45:55 +0300

    done lessons 114-117

--- js/lessons.js
+++ js/lessons.js
@@ -2393,3 +2393,61 @@
 //         phoneAnimation.pause();
 //     }
 // })
+
+
+
+// Lesson 114
+
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 4000);
+
+
+// setTimeout(() => {
+//     console.log('timeout_4000');
+// }, 4000);
+
+// console.log(2);
+
+// let k = 0;
+
+// function count() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+
+//     alert('done');
+// }
+
+// count();
+
+// setTimeout(() => {
+//     console.log(1);
+// }, 0);
+
+// console.log(2);
+
+
+
+// Lesson 115
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }); 
+
+// Promise.resolve()
+//     .then(() => console.log('promise'));
+
+// queueMicrotask(() => console.log('wow'));
+
+// Promise.resolve()
+//     .then(() => console.log('promise2'));
+
+// console.log('code');
+
+
+ 
+
+ 
