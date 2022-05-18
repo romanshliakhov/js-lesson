c4eeef2e4924a736067ca875d016b5e720fd1518
Roman <romanshliakhov@gmail.com>
2022-05-18 10:51:52 +0300

    done lesson 127

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,23 @@
-import {Component} from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello World</h2>
-}
-
-// const Field = () => {
-//   const holder = 'Enter here';
-//   const styledField = {
-//     width: '300px'
-//   };
-//   return <input 
-//           placeholder={holder} 
-//           type='text' 
-//           style={styledField}/>
-// }
-
-class Field extends Component {
-  render() {
-    const holder = 'Enter here';
-    const styledField = {
-        width: '300px'
-    };
-
-    return <input 
-    placeholder={holder} 
-    type='text' 
-    style={styledField}/>
-  }
-}
-
-function Btn() {
-  const text = 'Log in';
-  const logged = false;
-
-  return <button>{logged ? 'Enter' : text}</button>
+function WhoAmI ({name, surname, link}) { 
+  return (
+    <div>
+      <h1>My name is {name()}, surname - {surname}</h1> 
+      <a href={link}>My profile</a>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Field/>
-      <Btn/>
+        <WhoAmI name={() => {return "John"}} surname="Smith" link="#"/>
+        <WhoAmI name={() => {return "Alex"}} surname="Walters" link="#"/>
     </div>
   );
 }
 
-export {Header};
+
+
 export default App;
